ea7c5f68eac7508d9785ca5f32d65ff4ffce3bd4
SSDUMA11 <dumasofia2@gmail.com>
2023-05-16 10:40:39 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'home-work';
+  submit(form: any){
+    if(form.valid){
+      console.log(form.value);
+    } else {
+      form.control.markAllAsTouched()
+    }
+  }
+
+
 }
